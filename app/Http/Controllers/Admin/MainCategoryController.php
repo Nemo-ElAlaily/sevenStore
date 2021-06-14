897e44a8dea574c2c39af15816d4cf586026b871
Nemo-ElAlaily <tassnim.elalaily@gmail.com>
2021-06-14 15:57:24 +0200

    main category controller done

--- app/Http/Controllers/Admin/MainCategoryController.php
+++ app/Http/Controllers/Admin/MainCategoryController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\MainCategory;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class MainCategoryController extends Controller
 {
@@ -24,31 +27,162 @@ class MainCategoryController extends Controller
 
     public function create()
     {
+        try {
+            return view('admin.cuba.main_categories.create');
 
+        } catch (\Exception $exception) {
+
+            session()->flash('error', 'Something Went Wrong, Please Contact Administrator');
+            return redirect()->route('admin.main_categories.index' );
+
+        } // end of try & catch
     } //end of create
 
     public function store($id, Request $request)
     {
+        try {
+            DB::beginTransaction();
+
+            $imagePath = "";
+            if($request -> image){
+                $imagePath = uploadImage('uploads/main_categories/' . Carbon::now()-> year . '/' . Carbon::now() -> month . '/',  $request -> image);
+            } else {
+                $imagePath = Carbon::now()-> year . '/' . Carbon::now() -> month . '/' . 'default.png';
+            }
+
+            $category =  MainCategory::create([
+                'name' => $request -> name,
+                'slug' => str_replace( [' ', '/'], '_', $request -> name),
+                'parent_id' => $request -> parent_id,
+                'image' => $imagePath,
+            ]);
+
+            DB::commit();
+
+            session()->flash('success', 'Category Added Successfully');
+            return redirect()->route('admin.main_categories.index');
+
+        } catch (\Exception $exception) {
+            //return $exception;
+            DB::rollback();
+            session()->flash('error', 'Something Went Wrong, Please Contact Administrator');
+            return redirect()->route('admin.main_categories.index');
+
+        } // end of try & catch
 
     } //end of store
 
     public function show($id)
     {
+        try {
+            return $main_category = MainCategory::find($id);
+
+            if(!$main_category){
+                session()->flash('error', "Category ID Doesn't Exist or has been deleted");
+                return redirect()->route('admin.main_categories.index');
+            }
+
+            return view('admin.cuba.main_categories.show', compact('$main_category'));
+
+        } catch (\Exception $exception) {
+
+            session()->flash('error', 'Something Went Wrong, Please Contact Administrator');
+            return redirect()->route('admin.main_categories.index');
+
+        }
 
     } //end of show
 
     public function edit($id)
     {
+        try {
+            $main_category = MainCategory::find($id);
+
+            if(!$main_category){
+                session()->flash('error', "Category ID Doesn't Exist or has been deleted");
+                return redirect()->route('admin.main_categories.index');
+            }
+
+            return view('admin.cuba.main_categories.edit', compact('$main_category'));
+
+        } catch (\Exception $exception) {
+
+            session()->flash('error', 'Something Went Wrong, Please Contact Administrator');
+            return redirect()->route('admin.main_categories.index');
+
+        }
 
     } //end of edit
 
     public function update($id ,Request $request)
     {
+        try {
+            $main_category = MainCategory::find($id);
+
+            if(!$main_category){
+                session()->flash('error', "Product Doesn't Exist or has been deleted");
+                return redirect()->route('admin.products.index');
+            }
+
+            DB::beginTransaction();
+
+            $imagePath = "";
+            if($request->image){
+                if ($main_category -> image != 'default.png') {
+                    Storage::disk('public_uploads')->delete('uploads/main_categories/' . $main_category -> image);
+                } // end of inner if
+                $imagePath = uploadImage('uploads/main_categories',  $request -> image);
+            } else {
+                $imagePath = $main_category -> image_path;
+            }// end of outer if
+
+            $main_category -> update([
+                'name' => $request -> name,
+                'slug' => str_replace( [' ', '/'], '_', $request -> name),
+                'parent_id' => $request -> parent_id,
+                'image' => $imagePath,
+            ]);
+
+            DB::commit();
+
+            session()->flash('success', 'Category Updated Successfully');
+            return redirect()->route('admin.main_categories.index');
+
+        } catch (\Exception $exception) {
+
+            DB::rollback();
+            session()->flash('error', 'Something Went Wrong, Please Contact Administrator');
+            return redirect()->route('admin.main_categories.index');
+
+        } // end of try & catch
 
     } //end of update
 
     public function destroy($id)
     {
+        $main_category = MainCategory::find($id);
+
+        if(!$main_category){
+            session()->flash('error', "Category ID Doesn't Exist or has been deleted");
+            return redirect()->route('admin.main_categories.index');
+        }
+
+        try {
+            if ($main_category -> image != 'default.png') {
+                Storage::disk('public_uploads')->delete('uploads/main_categories/' . $main_category -> image);
+            } // end of inner if
+
+            $main_category -> delete();
+
+            session()->flash('success', 'Category Deleted Successfully');
+            return redirect()->route('admin.main_categories.index');
+
+        } catch (\Exception $exception) {
+
+            session()->flash('error', 'Something Went Wrong, Please Contact Administrator');
+            return redirect()->route('admin.main_categories.index');
+
+        } // end of try & catch
 
     } //end of destroy
 
