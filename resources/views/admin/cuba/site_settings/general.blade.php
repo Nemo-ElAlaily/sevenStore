dce88bbbe52822418e2372f2390bbb1b0a640a4e
MuhammadHamdy77 <hamaki.m91@yahoo.com>
2021-06-16 14:26:13 +0200

    general setting

--- resources/views/admin/cuba/site_settings/general.blade.php
+++ resources/views/admin/cuba/site_settings/general.blade.php
@@ -21,10 +21,10 @@
             <div class="row">
                 <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
                     <li class="nav-item">
-                      <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">LTR</a>
+                      <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">عربي </a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">RTL</a>
+                      <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">En</a>
                     </li>
                   </ul>
                   <div class="tab-content" id="pills-tabContent">
@@ -51,64 +51,64 @@
                         @foreach (config('translatable.locales') as $locale)
                             <div class="col-sm-12 col-lg-6">
                                 <div class="form-group">
-                                    <label for="{{ $locale }}">Title in @lang('site.' . $locale .
+                                  <label class="bg-warning my-2"   for="{{ $locale }}">Title in @lang('site.' . $locale .
                                         '.name')</label>
                                     @error($locale . '.title')
                                     <br />
                                     <span class="text-danger mx-5">{{ $message }}</span>
                                     @enderror
-                                    <input class="form-control input-thick" type="text" name="{{ $locale }}[title]"
+                                    <input class="form-control  input-thick bg-dark text-center" type="text" name="{{ $locale }}[title]"
                                            value="{{ $site_settings->translate($locale)->title }}">
                                 </div>
                                 <div class="form-group">
-                                    <label for="{{ $locale }}">Welcome Phrase in @lang('site.' . $locale .
+                                  <label class="bg-warning my-2"   for="{{ $locale }}">Welcome Phrase in @lang('site.' . $locale .
                                         '.name')</label>
                                     @error($locale . '.welcome_phrase')
                                     <br />
                                     <span class="text-danger mx-5">{{ $message }}</span>
                                     @enderror
-                                    <input class="form-control input-thick" type="text" name="{{ $locale }}[welcome_phrase]"
+                                    <input class="form-control  input-thick bg-dark text-center" type="text" name="{{ $locale }}[welcome_phrase]"
                                            value="{{ $site_settings->translate($locale)->welcome_phrase }}">
                                 </div>
                                 <div class="form-group">
-                                    <label for="{{ $locale }}">Title in @lang('site.' . $locale .
+                                  <label class="bg-warning my-2"   for="{{ $locale }}">Title in @lang('site.' . $locale .
                                         '.name')</label>
                                     @error($locale . '.address')
                                     <br />
                                     <span class="text-danger mx-5">{{ $message }}</span>
                                     @enderror
-                                    <input class="form-control input-thick" type="text" name="{{ $locale }}[address]"
+                                    <input class="form-control  input-thick bg-dark text-center" type="text" name="{{ $locale }}[address]"
                                            value="{{ $site_settings->translate($locale)->address }}">
                                 </div>
                                 <div class="form-group">
-                                    <label for="{{ $locale }}">Title in @lang('site.' . $locale .
+                                  <label class="bg-warning my-2"   for="{{ $locale }}">Title in @lang('site.' . $locale .
                                         '.name')</label>
                                     @error($locale . '.city')
                                     <br />
                                     <span class="text-danger mx-5">{{ $message }}</span>
                                     @enderror
-                                    <input class="form-control input-thick" type="text" name="{{ $locale }}[city]"
+                                    <input class="form-control  input-thick bg-dark text-center" type="text" name="{{ $locale }}[city]"
                                            value="{{ $site_settings->translate($locale)->city }}">
                                 </div>
                                 <div class="form-group">
-                                    <label for="{{ $locale }}">Title in @lang('site.' . $locale .
+                                  <label class="bg-warning my-2"   for="{{ $locale }}">Title in @lang('site.' . $locale .
                                         '.name')</label>
                                     @error($locale . '.country')
                                     <br />
                                     <span class="text-danger mx-5">{{ $message }}</span>
                                     @enderror
-                                    <input class="form-control input-thick" type="text" name="{{ $locale }}[country]"
+                                    <input class="form-control  input-thick bg-dark text-center" type="text" name="{{ $locale }}[country]"
                                            value="{{ $site_settings->translate($locale)->country }}">
                                 </div>
 
                                 <div class="form-group">
-                                    <label for="{{ $locale }}[meta_title]">Meta Title in @lang('site.' .
+                                  <label class="bg-warning my-2"   for="{{ $locale }}[meta_title]">Meta Title in @lang('site.' .
                                         $locale . '.meta_title')</label>
                                     @error($locale . '.meta_title')
                                     <br />
                                     <span class="text-danger mx-5">{{ $message }}</span>
                                     @enderror
-                                    <input class="form-control input-thick" type="text"
+                                    <input class="form-control  input-thick bg-dark text-center" type="text"
                                            name="{{ $locale }}[meta_title]"
                                            value="{{ $site_settings->translate($locale)->meta_title }}">
                                 </div>
@@ -121,26 +121,26 @@
                         @foreach (config('translatable.locales') as $locale)
                             <div class="col-sm-12 col-lg-6">
                                 <div class="form-group">
-                                    <label for="{{ $locale }}[meta_description]">Meta Description in @lang('site.' .
+                                  <label class="bg-warning my-2"   for="{{ $locale }}[meta_description]">Meta Description in @lang('site.' .
                                         $locale .
                                         '.meta_description')</label>
                                     @error($locale . '.meta_description')
                                     <br />
                                     <span class="text-danger mx-5">{{ $message }}</span>
                                     @enderror
-                                    <input class="form-control input-thick" type="text"
+                                    <input class="form-control  input-thick bg-dark text-center" type="text"
                                            name="{{ $locale }}[meta_description]"
                                            value="{{ $site_settings->translate($locale)->meta_description }}">
                                 </div>
 
                                 <div class="form-group">
-                                    <label for="{{ $locale }}[meta_keyword]">Meta Keyword in @lang('site.' .
+                                  <label class="bg-warning my-2"   for="{{ $locale }}[meta_keyword]">Meta Keyword in @lang('site.' .
                                         $locale . '.meta_keyword')</label>
                                     @error($locale . '.meta_keyword')
                                     <br />
                                     <span class="text-danger mx-5">{{ $message }}</span>
                                     @enderror
-                                    <input class="form-control input-thick" type="text"
+                                    <input class="form-control  input-thick bg-dark text-center" type="text"
                                            name="{{ $locale }}[meta_keyword]"
                                            value="{{ $site_settings->translate($locale)->meta_keyword }}">
                                 </div>
@@ -149,7 +149,7 @@
                         @endforeach
                                 
                         <div class="form-group col-sm-12 col-lg-12">
-                            <label>Logo</label>
+                          <label class="bg-warning my-2"  >Logo</label>
                             @error('logo')
                             <span class="text-danger mx-1">{{ $message }}</span>
                             @enderror
