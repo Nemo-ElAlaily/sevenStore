a43f182afad855839506b85ce373052940e59f46
Nemo-ElAlaily <tassnim.elalaily@gmail.com>
2021-06-17 17:57:32 +0200

    adjust database table view

--- resources/views/admin/cuba/site_settings/database.blade.php
+++ resources/views/admin/cuba/site_settings/database.blade.php
@@ -17,12 +17,12 @@
 
             @include('admin.cuba.partials._session')
             @include('admin.cuba.partials._errors')
-    
+
             <form class="col-12"  action="{{ route('admin.settings.database.update', 1) }}" method="POST" enctype="multipart/form-data">
-    
+
                 {{ csrf_field() }}
                 {{ method_field('PUT') }}
-    
+
                 <div class="row card">
                     <div class="col-sm-12 row">
                         <table class="text-center pt-2 bg-white table table-hover table-bordered">
@@ -34,9 +34,9 @@
                                     <th class="text-dark">Notes</th>
                                 </tr>
                             </thead>
-    
+
                             <tbody>
-    
+
                                 <tr>
                                     <td class="align-center text-dark">URL</td>
                                     <td>
@@ -45,17 +45,17 @@
                                                    value="{{ $database_settings -> DATABASE_URL }}">
                                         </div>
                                     </td>
-    
+
                                     <td>
                                         <div class="form-group">
                                             <input class="form-control input-thick" type="text" name="WP_DATABASE_URL"
                                                    value="{{ $database_settings -> WP_DATABASE_URL }}">
                                         </div>
                                     </td>
-    
+
                                     <td></td>
                                 </tr>
-    
+
                                 <tr>
                                     <td class="align-center text-dark">Connection</td>
                                     <td>
@@ -67,7 +67,7 @@
                                             @enderror
                                         </div>
                                     </td>
-    
+
                                     <td>
                                         <div class="form-group">
                                             <input class="form-control input-thick" type="text" name="WP_DB_CONNECTION"
@@ -77,10 +77,10 @@
                                             @enderror
                                         </div>
                                     </td>
-    
+
                                     <td></td>
                                 </tr>
-    
+
                                 <tr>
                                     <td class="align-center text-dark">Host</td>
                                     <td>
@@ -92,7 +92,7 @@
                                             @enderror
                                         </div>
                                     </td>
-    
+
                                     <td>
                                         <div class="form-group">
                                             <input class="form-control input-thick" type="text" name="WP_DB_HOST"
@@ -102,10 +102,10 @@
                                             @enderror
                                         </div>
                                     </td>
-    
+
                                     <td></td>
                                 </tr>
-    
+
                                 <tr>
                                     <td class="align-center text-dark">WP_DB_PORT</td>
                                     <td>
@@ -117,7 +117,7 @@
                                             @enderror
                                         </div>
                                     </td>
-    
+
                                     <td>
                                         <div class="form-group">
                                             <input class="form-control input-thick" type="text" name="WP_DB_PORT"
@@ -129,7 +129,7 @@
                                     </td>
                                     <td></td>
                                 </tr>
-    
+
                                 <tr>
                                     <td class="align-center text-dark">Database Name</td>
                                     <td>
@@ -141,7 +141,7 @@
                                             @enderror
                                         </div>
                                     </td>
-    
+
                                     <td>
                                         <div class="form-group">
                                             <input class="form-control input-thick" type="text" name="WP_DB_DATABASE"
@@ -151,23 +151,23 @@
                                             @enderror
                                         </div>
                                     </td>
-    
+
                                     <td></td>
                                 </tr>
-    
+
                                 <tr>
                                     <td class="align-center text-dark">Database Username</td>
                                     <td>
                                         <div class="form-group">
                                             <input class="form-control input-thick" type="text" name="DB_USERNAME"
                                                    value="{{ $database_settings -> DB_USERNAME }}">
-    
+
                                             @error('DB_USERNAME')
                                             <span class="text-danger mx-1" >{{ $message }}</span>
                                             @enderror
                                         </div>
                                     </td>
-    
+
                                     <td>
                                         <div class="form-group">
                                             <input class="form-control input-thick" type="text" name="WP_DB_USERNAME"
@@ -177,10 +177,10 @@
                                             @enderror
                                         </div>
                                     </td>
-    
+
                                     <td></td>
                                 </tr>
-    
+
                                 <tr>
                                     <td class="align-center text-dark">Password</td>
                                     <td>
@@ -192,7 +192,7 @@
                                             @enderror
                                         </div>
                                     </td>
-    
+
                                     <td>
                                         <div class="form-group">
                                             <input type="password" class="form-control input-thick" type="text" name="WP_DB_PASSWORD"
@@ -202,24 +202,24 @@
                                             @enderror
                                         </div>
                                     </td>
-    
+
                                     <td></td>
                                 </tr>
-    
+
                                 <div class="row py-5 m-auto text-center">
-    
+
                                       <div class="col-md-4">
                                           <div class="form-group m-6">
                                               <button type="submit" class="btn btnEdit "><i class="fa fa-edit"></i>
                                                   Update Database Credentials</button>
                                           </div>
-    
+
                                           <div class="form-group m-5">
                                               <a href="{{ route('admin.settings.database.migration', 1) }}" class="btn btnImport"><i class="fa fa-check-circle"></i>
                                                   Import Data</a>
                                           </div>
                                       </div>
-    
+
                                     <div class="col-md-6 alert-database">
                                         <h6>Requirements:</h6>
                                         <p class="text-bold">
@@ -227,18 +227,20 @@
                                             <br>
                                             Change max_input_time in "php.ini" file to 1200 sec.
                                             <br>
+                                            Change memory_limit	 in "php.ini" file to 250M.
+                                            <br>
                                         </p>
                                     </div>
                                 </div>
-    
+
                             </tbody>
                         </table><!-- end of table -->
-    
+
                     </div>
                 </div>
-    
+
             </form>
-    
+
         </div>
     </div>
 @stop
