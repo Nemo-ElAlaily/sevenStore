a9c5eea59ab1c124b36ffc28eb11dad9f325f69f
Nemo-ElAlaily <tassnim.elalaily@gmail.com>
2021-06-22 16:28:49 +0200

    update database file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         $this->call(ProductsTableSeeder::class);
         $this->call(OrdersTableSeeder::class);
         $this->call(PagesTableSeeder::class);
+        $this->call(BlogsTableSeeder::class);
     }
 }
