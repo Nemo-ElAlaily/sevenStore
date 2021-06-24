0a3bd46c0a5585903ad331ea6bf3aac07bf859d4
MuhammedHamdy77 <MuhammadHamdy77>
2021-06-24 17:51:55 +0200

    Change Style Form Wizard

--- resources/views/admin/cuba/forms/db-new.blade.php
+++ resources/views/admin/cuba/forms/db-new.blade.php
@@ -3,72 +3,82 @@
 @section('title', 'Form Wizard')
 
 @section('content')
-    <div class="container-fluid">
+    <div class="container-fluid pt-5">
         <div class="row">
             <div class="col-md-9 m-auto">
                 <div class="card cardwizard" style="margin: auto">
-                    <div class="card-header">
-                        <p class="alert alertTabs text-center">Please Make sure fill all the filed before click on next button</p>
-                    </div>
+                <div class="overlay">
+                    {{-- <div class="alert alertTabs ">
+                        <p class="text-center">Please Make sure fill all the filed before click on next button</p>
+                    </div> --}}
                     <div class="card-body">
+                        <div class="imgTabs"> 
+                            <img src="{{asset('admins/cuba/assets/images/favicon.png')}}" />
+                        </div>
                         <form class="form-wizard" id="regForm" action="{{ route('admin.settings.database.store') }}" method="POST">
 
                             {{ csrf_field() }}
                             {{ method_field('post') }}
 
-                            <div class="tab">
+                            <div class="tabsShadow">
+                            
+                                <div class="tab">
 
-                                    <div class="imgTabs">
-
-                                        <img src="{{asset('admins/cuba/assets/images/db.jpg')}}" />
-                                            <h5>New Database</h5>
-                                    </div>
-
-                                <div class="mb-3">
-                                    <label for="DB_DATABASE">New Database Name</label>
-                                    <input class="form-control" id="DB_DATABASE" type="text" placeholder="Database Name" required="required">
+                                <div class="mb-3 input-container   ">
+                                    {{-- <label class="titel-db" for="DB_DATABASE">New Database Name</label> --}}
+                                        <i class="fa fa-user icon"></i>
+                                    <input class="form-control" id="DB_DATABASE" type="text" placeholder="New Database Name" required="required">
                                 </div>
-                                <div class="mb-3">
-                                    <label for="DB_HOST">New Database Host</label>
-                                    <input class="form-control" id="DB_HOST" type="text" placeholder="host">
+                                <div class="mb-3 input-container  ">
+                                    {{-- <label class="titel-db" for="DB_HOST">New Database Host</label> --}}
+                                    <i class="fa fa-user icon"></i>
+                                    <input class="form-control" id="DB_HOST" type="text" placeholder="New Database Host">
                                 </div>
-                                <div class="mb-3">
-                                    <label for="DB_USERNAME">New Database Username</label>
-                                    <input class="form-control" id="DB_USERNAME" type="url" placeholder="Database Username">
+                                <div class="mb-3 input-container  ">
+                                    {{-- <label class="titel-db" for="DB_USERNAME">New Database Username</label> --}}
+                                    <i class="fa fa-user icon"></i>
+                                    <input class="form-control" id="DB_USERNAME" type="url" placeholder=" New Database Username">
                                 </div>
-                                <div class="mb-3">
-                                    <label for="DB_PASSWORD">New Database Password</label>
-                                    <input class="form-control" id="DB_PASSWORD" type="password" placeholder="Database Password">
+                                <div class="mb-3 input-container  ">
+                                    {{-- <label class="titel-db" for="DB_PASSWORD">New Database Password</label> --}}
+                                    <i class="fa fa-key icon"></i>
+                                    <input class="form-control" id="DB_PASSWORD" type="password" placeholder="New Database Password ">
                                 </div>
                             </div>
 
                              <div class="tab">
 
-                                    <div class="imgTabs">
+                                    {{-- <div class="imgTabs"> --}}
 
-                                        <img src="{{asset('admins/cuba/assets/images/wordpress.png')}}" />
-                                        <h5>Wordpress Database</h5>
-                                    </div>
+                                        {{-- <img src="{{asset('admins/cuba/assets/images/wordpress.png')}}" /> --}}
+                                        {{-- <h5>Wordpress Database</h5>
+                                    </div> --}}
 
-                                <div class="mb-3">
-                                    <label for="WP_DB_DATABASE">Wordpress Database Name</label>
+                                <div class="mb-3 input-container  ">
+                                    {{-- <label class="titel-db" for="WP_DB_DATABASE">Wordpress Database Name</label> --}}
+                                    <i class="fa fa-user icon"></i>
                                     <input class="form-control" id="WP_DB_DATABASE" type="text" placeholder="Wordpress Database Name" required="required">
                                 </div>
-                                <div class="mb-3">
-                                    <label for="WP_DB_HOST">Wordpress Database Host</label>
+                                <div class="mb-3 input-container  ">
+                                    {{-- <label class="titel-db" for="WP_DB_HOST">Wordpress Database Host</label> --}}
+                                    <i class="fa fa-user icon"></i>
                                     <input class="form-control" id="WP_DB_HOST" type="text" placeholder="Wordpress Database Host">
                                 </div>
-                                <div class="mb-3">
-                                    <label for="WP_DB_USERNAME">Wordpress Database Username</label>
+                                <div class="mb-3 input-container  ">
+                                    {{-- <label class="titel-db" for="WP_DB_USERNAME">Wordpress Database Username</label> --}}
+                                    <i class="fa fa-user icon"></i>
                                     <input class="form-control" id="WP_DB_USERNAME" type="url" placeholder="Wordpress Database Username">
                                 </div>
-                                <div class="mb-3">
-                                    <label for="WP_DB_PASSWORD">Wordpress Database Password</label>
+                                <div class="mb-3 input-container  ">
+                                    {{-- <label class="titel-db" for="WP_DB_PASSWORD">Wordpress Database Password</label> --}}
+                                    <i class="fa fa-key icon"></i>
                                     <input class="form-control" id="WP_DB_PASSWORD" type="password" placeholder="Wordpress Database Password">
                                 </div>
                             </div>
 
 
+
+                            </div>
                             <div>
                                 <div class="text-end btn-mb">
                                     <button class="btn btnPrev" id="prevBtn" type="button" onclick="nextPrev(-1)">Previous</button>
@@ -83,6 +93,7 @@
                             </div>
                             <!-- Circles which indicates the steps of the form:-->
                         </form>
+                    </div>
                     </div>
                 </div>
             </div>
