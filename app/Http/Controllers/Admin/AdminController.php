5c0eec1a22b1ea6cac8fc8b1f11b6544cbbaa146
Nemo-ElAlaily <tassnim.elalaily@gmail.com>
2021-07-01 13:48:40 +0200

    update dashboard view

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Blogs\Blog;
+use App\Models\Orders\Order;
+use App\Models\Products\Product;
+use App\Models\User;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin.cuba.index');
+        $orders_count = Order::where('status', ['on-hold', 'processing'])->count();
+        $products_count = Product::where('status', 'publish')->count();
+        $blog_count = Blog::count();
+        $users_count = User::whereRoleIs('user')->count();
+        return view('admin.cuba.index' , compact('orders_count', 'products_count', 'blog_count', 'users_count'));
 
     }// end of index
 
--- resources/views/admin/cuba/index.blade.php
+++ resources/views/admin/cuba/index.blade.php
@@ -8,31 +8,31 @@
     <div class="col-md-3">
       <div class="card-counter primary">
         <i class="fa fa-code-fork"></i>
-        <span class="count-numbers">12</span>
-        <span class="count-name">Flowz</span>
+        <span class="count-numbers">{{ $orders_count }}</span>
+        <span class="count-name">Order</span>
       </div>
     </div>
 
     <div class="col-md-3">
       <div class="card-counter danger">
         <i class="fa fa-ticket"></i>
-        <span class="count-numbers">599</span>
-        <span class="count-name">Instances</span>
+        <span class="count-numbers">{{ $products_count }}</span>
+        <span class="count-name">Product</span>
       </div>
     </div>
 
     <div class="col-md-3">
       <div class="card-counter success">
         <i class="fa fa-database"></i>
-        <span class="count-numbers">6875</span>
-        <span class="count-name">Data</span>
+        <span class="count-numbers">{{ $blog_count }}</span>
+        <span class="count-name">Blog</span>
       </div>
     </div>
 
     <div class="col-md-3">
       <div class="card-counter info">
         <i class="fa fa-users"></i>
-        <span class="count-numbers">35</span>
+        <span class="count-numbers">{{ $users_count }}</span>
         <span class="count-name">Users</span>
       </div>
     </div>
