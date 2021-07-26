302dba0924541023f0be5118859aa17690636912
Nemo-ElAlaily <tassnim.elalaily@gmail.com>
2021-07-26 16:17:09 +0200

    navbar pages links adjustment

--- resources/views/front/includes/navigation/primary-nav.blade.php
+++ resources/views/front/includes/navigation/primary-nav.blade.php
@@ -1,5 +1,7 @@
 <?php
     $navbar_pages =  App\Models\Pages\Page::where([['is_active','1'],['show_in_navbar','1']])->get();
+
+
 ?>
 <div class="clearfix">
     <button class="navbar-toggler hidden-sm-up pull-right flip" type="button" data-toggle="collapse"
@@ -11,7 +13,7 @@
 <div class="collapse navbar-toggleable-xs" id="header-v3">
     <ul class="nav navbar-nav">
           <!-- Dropdown -->
-    <li class="nav-item dropdown menu-item m-auto">
+    <li class="menu-item m-auto dropdown mx-4">
       <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">{{ trans('site.Pages') }}</a>
       <div class="dropdown-menu">
       @foreach ($navbar_pages as $page)
@@ -21,7 +23,7 @@
 
       </div>
     </li>
-        @foreach ($main_categories->where('parent_id', 0)->slice(1, LaravelLocalization::getCurrentLocale() == 'ar' ? 7 : 8) as $main_category)
+        @foreach ($main_categories->where('parent_id', 0)->slice(1, LaravelLocalization::getCurrentLocale() == 'ar' ? 7 : 9) as $main_category)
             <li class="menu-item m-auto">
                 <a title="Gadgets"
                     href="{{ route('front.product.category', $main_category->slug) }}">{{ $main_category->name }}</a>
